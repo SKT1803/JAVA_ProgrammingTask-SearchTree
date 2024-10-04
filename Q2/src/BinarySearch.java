942512bcce79c89c4177dba555950980256f4668
SKT1803 <skemalt@outlook.com>
2024-10-04 16:58:44 +0300

    Update BinarySearch.java

--- Q2/src/BinarySearch.java
+++ Q2/src/BinarySearch.java
@@ -1,9 +1,6 @@
 //-----------------------------------------------------
 // Title: CaptainDatabase class
-// Author: Serdar Kemal Topkaya / Burak Eren Birinci
-// ID: 71200082370 / 36596076990
-// Section: 3
-// Assignment: 3
+// Author: Serdar Kemal Topkaya
 // Description: This class creates a binary search tree that name is CaptainDatabase(BinarySearch).
 // It has methods which are addCaptain (adds the captain in the tree(database)), 
 // searchCaptain (search the captain in the tree), deleteCaptain (deletes the captain from the tree),  
